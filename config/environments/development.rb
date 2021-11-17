9049f623430f5946e322213996a8228e70ccc3ac
bhanutejaror <bhanutejaror@gmail.com>
2021-11-17 13:20:38 +0000

    iamge production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 config.hosts << "2519a3ade5884d05b31446fa36e8075b.vfs.cloud9.ap-south-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
